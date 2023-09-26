b5175fe9657d45049b10d476f71f284afe3198bd
erwann <erwann2002.ec@gmail.com>
2023-09-26 15:48:54 +0200

    encore plus de com

--- TP2/planning/DijkstraPlanner.java
+++ TP2/planning/DijkstraPlanner.java
@@ -19,10 +19,12 @@ public class DijkstraPlanner implements Planner {
 
     /**
      * Construit un DijkstraPlanner avec un état initial, un ensemble d'actions et l'objectif à atteindre
+     * Le but de ce planificateur est de trouver le cout minimal du chemin
      * 
      * @param initialState L'état initial du problème
      * @param actions      L'ensemble d'actions disponibles qui peuvent être appliquées aux états
      * @param goal         L'état objectif que le planificateur vise à atteindre
+     * @return            Une liste d'actions représentant le plan pour atteindre l'objectif ou null si aucun plan n'est trouvé
      */
     public DijkstraPlanner(Map<Variable, Object> initialState, Set<Action> actions, Goal goal) {
         this.initialState = initialState;
@@ -31,7 +33,7 @@ public class DijkstraPlanner implements Planner {
         this.nombresNoeuds = 0;
     }
 
-
+    // On prend en paramètre un plan, une distance et un père et retourne une liste d'actions
     public List<Action> dijkstra(Map<Map<Variable, Object>, Action> plan, Map<Map<Variable, Object>, Float> distance, Map<Map<Variable, Object>, Map<Variable, Object>> father) {
 
         // on initialise la liste des objectifs avec une file de priorité
@@ -48,10 +50,11 @@ public class DijkstraPlanner implements Planner {
         
         // tant que la liste des ouverts n'est pas vide
         while (!open.isEmpty()) {
-            //on récupère l'état actuel
+
+            //on récupère l'état actuel et on le retire de la liste des ouverts
             Map<Variable, Object> currentState = open.poll();
 
-            // on vérifie si l'état actuel satisfait l'objectif
+            // on vérifie si l'état récupéré satisfait l'objectif
             if (goal.isSatisfiedBy(currentState)) {
                 // si oui on ajoute l'état actuel à la liste des objectifs
                 goals.add(currentState);
@@ -59,8 +62,12 @@ public class DijkstraPlanner implements Planner {
 
             // si l'état courant n'est pas l'état objectif, on parcourt les toutes les actions possibles
             for (Action action : actions) {
+                // on vérifie si l'action est applicable à l'état actuel
                 if (action.isApplicable(currentState)) {
+                    // on récupère l'état suivant
                     Map<Variable, Object> nextState = action.successor(currentState);
+
+                    // on vérifie si l'état suivant n'est pas déjà dans la liste des distances
                     if (!distance.containsKey(nextState)) {
                         distance.put(nextState, Float.MAX_VALUE);
                     }
@@ -77,10 +84,11 @@ public class DijkstraPlanner implements Planner {
                 }
             }
         }
+        //si aucune solution n'a été trouvée on retourne null
         if (goals.isEmpty()){
             return null;
         }
-        // on retourne le plan
+        // sinon on retourne le plan en utilisant une recherche en largeur
         return getPlan(father, plan, goals, distance);
     }
 
@@ -94,18 +102,25 @@ public class DijkstraPlanner implements Planner {
      * @return Une liste d'actions représentant le plan final pour atteindre l'objectif.
      */
     public List<Action> getPlan(Map<Map<Variable, Object>, Map<Variable, Object>> father, Map<Map<Variable, Object>, Action> plan, Queue<Map<Variable, Object>> goals, Map<Map<Variable, Object>, Float> distance) {
-        //
+        //on commence par stocker le plans d'actions dans une liste 
         LinkedList<Action> dijkstraPlan = new LinkedList<>();
-        // On récupère le dernier objectif
+        //On récupère le premier élement de la file et on le supprime de la file
         Map<Variable, Object> goal = goals.poll();
+
         while (goal != null) {
             Action nextAction = plan.get(goal);
+            // S'il n'y a pas d'action associé à l'état courant, on sort de la boucle
             if (nextAction == null) {
                 break;
             }
+            // On met à jour la variable goal en remplacant l'état courant par son père
+            // Ca permet de remonter l'arbre des états
             goal = father.get(goal);
+            //on met le nextAction au début de la liste
+            //on obtient ainsi le plan dans le bon ordre (du début à la fin)
             dijkstraPlan.addFirst(nextAction);
             }
+        // Une fois que toutes les actions ont été ajoutées à la liste, on retourne le plan
         return dijkstraPlan;
     }
 
