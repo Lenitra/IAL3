84a1bd1383e939e65fe71ff38f97ddd0e2b2856e
Thomas <lenitramc@gmail.com>
2023-10-17 15:24:23 +0200

    [DomainSizeVariableHeuristicTests::testBest] passed

--- cp/DomainSizeVariableHeuristic.java
+++ cp/DomainSizeVariableHeuristic.java
@@ -7,17 +7,36 @@ import modelling.Variable;
 
 public class DomainSizeVariableHeuristic implements VariableHeuristic{
     
-
-
+    private boolean bool;
 
     public DomainSizeVariableHeuristic(boolean bool) {
-        
+        this.bool = bool;
     }
 
     @Override
-    public Variable best(Set<Variable> variables, Map<Variable, Set<Object>> domains) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'best'");
-    }
+    public Variable best(Set<Variable> variables, Map<Variable, Set<Object>> domaines) {
+        Variable best = null;
+        int sizeref = 0;
+        for (Variable var : variables) {
+            int sizedom = domaines.get(var).size();
 
+            if (best == null) {
+                best = var;
+                sizeref = sizedom;
+            } else {
+                if (bool) {
+                    if (sizedom > sizeref) {
+                        best = var;
+                        sizeref = sizedom;
+                    }
+                } else {
+                    if (sizedom < sizeref) {
+                        best = var;
+                        sizeref = sizedom;
+                    }
+                }
+            }
+        }
+        return best;
+    }
 }
