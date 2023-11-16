3803b60bcd4770a514490e65342e944d98a3397a
TauMah <lenitramc@gmail.com>
2023-11-16 10:01:32 +0100

    Add allActions method to BWPlanner class

--- blocksworld/BWPlanner.java
+++ blocksworld/BWPlanner.java
@@ -2,6 +2,7 @@ package blocksworld;
 
 import java.util.*;
 import modelling.*;
+import planning.Action;
 
 
 /**
@@ -11,12 +12,27 @@ public class BWPlanner {
 
     private int nbBlocks;
     private int nbPiles;
+    private 
 
     // constructor
-    public BWPlanner(int nbBlocks, int nbPiles) {
+    BWPlanner(int nbBlocks, int nbPiles) {
         this.nbBlocks = nbBlocks;
         this.nbPiles = nbPiles;
+        
     }
 
+
+    // get all actions
+    public ArrayList<Action> allActions() {
+        ArrayList<Action> actions = new ArrayList<Action>();
+        BWVariable variables = new BWVariable(this.nbBlocks, this.nbPiles); // On créé les variables
+        Set<Variable> variablesOn = variables.getOnb();
+        Set<Variable> variablesFixed = variables.getFixedb();
+        Set<Variable> variablesFree = variables.getFreep();
+
+    
+
+        return actions;
+    }
 
 }
