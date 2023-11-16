416adc2de28ded8fd20f517aa86e3a1a6f95c72b
TauMah <lenitramc@gmail.com>
2023-11-16 09:11:43 +0100

    Refactor blocksworld constraints and add tests

---
+++ blocksworld/BWPlanner.java
@@ -0,0 +1,23 @@
+package blocksworld;
+
+/**
+ * BWPlanner
+ */
+public class BWPlanner {
+
+    private int nbBlocks;
+    private int nbPiles;
+
+    // constructor
+    public BWPlanner(int nbBlocks, int nbPiles) {
+        this.nbBlocks = nbBlocks;
+        this.nbPiles = nbPiles;
+    }
+
+
+
+
+
+
+
+}
--- blocksworld/main.java
+++ blocksworld/main.java
@@ -6,104 +6,114 @@ import modelling.*;
 
 public class Main {
     
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             
-            BWConstraintes bwc = new BWRegularyConstraintes(4, 2);
-            BWConstraintes bwc2 = new BWRegularyConstraintes(3, 1);
+        boolean allSatisfied = true;
+        BWConstraintes bwc = new BWRegularyConstraintes(4, 2);
+        BWConstraintes bwc2 = new BWRegularyConstraintes(3, 1);
 
-            System.out.println(" ################# Test Regulary World #################");
+        System.out.println(" ################# Test Regulary World #################");
+        
+        Map<Variable, Object> map = new HashMap<>();
+        Map<Variable, Object> map2 = new HashMap<>();
 
-            Map<Variable, Object> map = new HashMap<>();
-            Map<Variable, Object> map2 = new HashMap<>();
-
-            for (Variable var : bwc.getBWVariable().getAllVars()) {
-                String name = var.getName();
-                if(name.equals("On_0")) {
-                    map.put(var, -1);
-                }
-
-                if(name.equals("On_1")) {
-                    map.put(var, -2);
-                }
-
-                if(name.equals("On_2")) {
-                    map.put(var, 0);
-                }
-
-                if(name.equals("On_3")) {
-                    map.put(var, 1);
-                }
-
-                if(name.equals("Fi_0")) {
-                    map.put(var, true);
-                }
-
-                if(name.equals("Fi_1")) {
-                    map.put(var, true);
-                }
-
-                if(name.equals("Fi_2")) {
-                    map.put(var, false);
-                }
-
-                if(name.equals("Fi_3")) {
-                    map.put(var, false);
-                }
-
-                if(name.equals("Fr_-1")) {
-                    map.put(var, false);
-                }
-
-                if(name.equals("Fr_-2")) {
-                    map.put(var, false);
-                }
+        for (Variable var : bwc.getBWVariable().getAllVars()) {
+            String name = var.getName();
+            if(name.equals("On_0")) {
+                map.put(var, -1);
             }
 
-            for (Variable var2 : bwc2.getBWVariable().getAllVars()) {
-                String name2 = var2.getName();
-                if (name2.equals("On_0")) {
-                    map2.put(var2, 1);
-                }
-
-                if (name2.equals("On_1")) {
-                    map2.put(var2, 2);
-                }
-
-                if (name2.equals("On_2")) {
-                    map2.put(var2, -1);
-                }
-
-                if (name2.equals("Fi_0")) {
-                    map2.put(var2, false);
-                }
-
-                if (name2.equals("Fi_1")) {
-                    map2.put(var2, true);
-                }
-
-                if(name2.equals("Fi_2")) {
-                    map2.put(var2, true);
-                }
-
-                if(name2.equals("Fr_-1")) {
-                    map2.put(var2, false);
-                }
+            if(name.equals("On_1")) {
+                map.put(var, -2);
             }
 
-            boolean allSatisfied = true;
-
-            for(Constraint cons : bwc.getConstraints()) {
-                if(!cons.isSatisfiedBy(map)) {
-                    allSatisfied = false;
-                }
+            if(name.equals("On_2")) {
+                map.put(var, 0);
             }
 
-            for(Constraint cons : bwc2.getConstraints()) {
-                if(!cons.isSatisfiedBy(map2)) {
-                    allSatisfied = false;
-                }
+            if(name.equals("On_3")) {
+                map.put(var, 1);
             }
 
-            System.out.println(allSatisfied ? "Constraints satisfied" : "Constraints unsatisfied");
-        }     
+            if(name.equals("Fi_0")) {
+                map.put(var, true);
+            }
+
+            if(name.equals("Fi_1")) {
+                map.put(var, true);
+            }
+
+            if(name.equals("Fi_2")) {
+                map.put(var, false);
+            }
+
+            if(name.equals("Fi_3")) {
+                map.put(var, false);
+            }
+
+            if(name.equals("Fr_-1")) {
+                map.put(var, false);
+            }
+            
+            if(name.equals("Fr_-2")) {
+                map.put(var, false);
+            }
+        }
+        
+        for(Constraint cons : bwc.getConstraints()) {
+            if(!cons.isSatisfiedBy(map)) {
+                allSatisfied = false;
+            }
+        }
+        
+        System.out.println(allSatisfied ? "test 1 OK" : "Test 1 pété");
+
+
+        for (Variable var2 : bwc2.getBWVariable().getAllVars()) {
+            String name2 = var2.getName();
+            if (name2.equals("On_0")) {
+                map2.put(var2, 1);
+            }
+
+            if (name2.equals("On_1")) {
+                map2.put(var2, 2);
+            }
+
+            if (name2.equals("On_2")) {
+                map2.put(var2, -1);
+            }
+
+            if (name2.equals("Fi_0")) {
+                map2.put(var2, false);
+            }
+
+            if (name2.equals("Fi_1")) {
+                map2.put(var2, true);
+            }
+
+            if(name2.equals("Fi_2")) {
+                map2.put(var2, true);
+            }
+
+            if(name2.equals("Fr_-1")) {
+                map2.put(var2, false);
+            }
+        }
+
+
+
+
+        for(Constraint cons : bwc2.getConstraints()) {
+            if(!cons.isSatisfiedBy(map2)) {
+                allSatisfied = false;
+            }
+        }
+        System.out.println(allSatisfied ? "test 2 OK" : "Test 2 pété");
+        
+        
+        
+        System.out.println(allSatisfied ? "Constraints satisfied" : "Constraints unsatisfied");
+        
+        
+    }
 }
