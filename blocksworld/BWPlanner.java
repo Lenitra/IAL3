84fa29c7c0e4b3e66dae1b624311d1c1d0b762a5
TauMah <lenitramc@gmail.com>
2023-11-16 11:56:30 +0100

    Toutes les actions son set (non testé)

--- blocksworld/BWPlanner.java
+++ blocksworld/BWPlanner.java
@@ -164,6 +164,44 @@ public class BWPlanner extends BlocksWorld {
             }
         }
 
+        // TYPE4
+        for (Variable bk1: variablesOn){
+            for (Variable p1: variablesFree){
+                for (Variable p2: variablesFree){
+                    if (p1.getName().equals(p2.getName())) { 
+                        continue;
+                    }
+
+                    int num1 = Integer.parseInt(bk1.getName().substring(2));
+                    int num2 = Integer.parseInt(p1.getName().substring(2));
+                    int num3 = Integer.parseInt(p2.getName().substring(2));
+
+                    // precondition :
+                    // On num1 = num2
+                    // Fi num1 = false
+                    // Fr num3 = true
+                    Map <Variable, Object> precondition = new HashMap<>();
+                    precondition.put(bk1, num2);
+                    precondition.put(variablesFixed.toArray(new Variable[variablesFixed.size()])[num1], false);
+                    precondition.put(variablesFree.toArray(new Variable[variablesFree.size()])[num3], true);
+
+                    // effects :
+                    // Fr num2 = true
+                    // Fr num3 = false
+                    // On num1.value = num3
+                    Map <Variable, Object> effect = new HashMap<>();
+                    effect.put(variablesFree.toArray(new Variable[variablesFree.size()])[num2], true);
+                    effect.put(variablesFree.toArray(new Variable[variablesFree.size()])[num3], false);
+                    effect.put(bk1, num3);
+
+                    // On créé l'action
+                    BasicAction action = new BasicAction(precondition, effect, 1);
+                    actions.add(action);
+
+                }
+            }
+        }
+
     
 
         return actions;
