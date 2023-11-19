132f5e6cf62d059977ec5588797f9f1032ab4dbf
erwann <erwann2002.ec@gmail.com>
2023-11-19 01:06:28 +0100

    blockonblock et blockon pile fonctionnel

--- blocksworld/BWActions2.java
+++ blocksworld/BWActions2.java
@@ -24,22 +24,22 @@ public class BWActions2 extends BlocksWorld {
         for (int block = 0; block < numBlocks; block++) {
             for(int sourceblock = 0; sourceblock < numBlocks; sourceblock++) {
                 for(int destiblock = 0; destiblock < numBlocks; destiblock++) {
-                    if (block != sourceblock || block != destiblock || sourceblock != destiblock) {
+                    if (block != sourceblock && block != destiblock && sourceblock != destiblock) {
                         BlockOnBlock(block, sourceblock, destiblock);
                     }
                 }
             }
     
-        // BLOCK VERS PILE
+            // BLOCK VERS PILE
             for(int sourceblock = 0; sourceblock < numBlocks; sourceblock++) {
-                for(int destipile = 1; destipile <= numPiles; destipile++) {
-                    if (block != destipile || sourceblock != destipile) {
+                for(int destipile = -1; destipile >= -numPiles; destipile--) {
+                    if (block != sourceblock) {
                         BlockOnPile(block, sourceblock, destipile);
                     }
                 }
             }
 
-        // BLOC SUR PILE VERS BLOC
+            // BLOC SUR PILE VERS BLOC
             for(int sourcePile = 0; sourcePile < numBlocks; sourcePile++) {
                 for(int destiblock = 0; destiblock < numBlocks; destiblock++) {
                     if (block != sourcePile || block != destiblock || sourcePile != destiblock) {
@@ -48,31 +48,43 @@ public class BWActions2 extends BlocksWorld {
                 }
             }
 
-        // BLOC SUR PILE VERS PILE
-            for(int sourceblock = 0; sourceblock < numBlocks; sourceblock++) {
+            // BLOC SUR PILE VERS PILE
+            for(int sourcePile = 0; sourcePile < numBlocks; sourcePile++) {
                 for(int destiblock = 0; destiblock < numPiles; destiblock++) {
-                    if (block != sourceblock || block != destiblock || sourceblock != destiblock) {
-                        BlockPileOnPile(block, sourceblock, destiblock);
+                    if (block != sourcePile || block != destiblock || sourcePile != destiblock) {
+                        BlockPileOnPile(block, sourcePile, destiblock);
                     }
                 }
             }
         }
     }
     
-
     public void BlockOnBlock(int block, int sourceblock, int destiblock){
         Map<Variable, Object> precondition = new HashMap<>();
         Variable varon = new Variable("On" + block, Set.of(sourceblock));
-
+        
+        //il faut que le bloc soit sur un autre bloc
         precondition.put(varon, sourceblock);
+        //il faut que le bloc ne soit pas fixé
         precondition.put(new BooleanVariable("Fi"+block), false);
-        precondition.put(new BooleanVariable("Fr"+sourceblock), false);
+        //il faut que le bloc de base soit fixé
+        precondition.put(new BooleanVariable("Fi"+sourceblock), true);
+        //il faut que le bloc d'arrivée soit pas fixé
+        precondition.put(new BooleanVariable("Fi"+destiblock), false);
+
+        // System.out.println("precon("+precondition.toString()+")");
 
         Map<Variable, Object> effect = new HashMap<>();
+        //le bloc est sur le bloc d'arrivée
         effect.put(varon, destiblock);
+        //le bloc n'est pas fixé
         effect.put(new BooleanVariable("Fi"+block), false);
+        //le bloc d'arrivée est fixé
         effect.put(new BooleanVariable("Fi"+destiblock), true);
-        effect.put(new BooleanVariable("Fr"+sourceblock), false);
+        //le bloc de base n'est plus occupé
+        effect.put(new BooleanVariable("Fi"+sourceblock), false);
+
+        System.out.println("BlockOnBlock("+block+","+sourceblock+","+destiblock+")");
 
         Action action = new BasicAction(precondition, effect, 1);
         actions.add(action);
@@ -89,21 +101,25 @@ public class BWActions2 extends BlocksWorld {
         //il faut que le bloc de base soit fixe
         precondition.put(new BooleanVariable("Fi"+sourceblock), true);
         //il faut que la pile d'arrivée soit libre
-        precondition.put(new BooleanVariable("Fr"+destiPile), true);
+        precondition.put(new BooleanVariable("Fr"+ -destiPile), true);
 
         //effet de l'action
         Map<Variable, Object> effect = new HashMap<>();
+        if (destiPile > 0){
+            destiPile = -destiPile;
+        }
         //le bloc est sur la pile d'arrivée
-        effect.put(varon, -destiPile);
+        effect.put(varon, destiPile);
         //le bloc n'est pas fixé
         effect.put(new BooleanVariable("Fi"+block), false);
         //la pile d'arrivée n'est plus libre
-        effect.put(new BooleanVariable("Fr"+destiPile), false);
+        effect.put(new BooleanVariable("Fr"+ -destiPile), false);
         //le bloc de base n'est plus occupé
         effect.put(new BooleanVariable("Fi"+sourceblock), false);
 
-        System.err.println("BlockOnPile("+block+","+sourceblock+","+destiPile+")");
+        // System.err.println("BlockOnPile("+block+","+sourceblock+","+destiPile+")");
 
+        
         Action action = new BasicAction(precondition, effect, 1);
         actions.add(action);
     }
--- blocksworld/MainActions.java
+++ blocksworld/MainActions.java
@@ -18,16 +18,21 @@ public class MainActions {
         // }
         // System.out.println();
 
-        testBlockOnPile();
+        System.out.println("Test BlockOnPile :");
+        testBlockOnPile(2,2);
  
+        System.out.println("Test BlockOnBlock :");
+        testBlockOnBlock(3,2);
+
+        // System.out.println("Test BlockPileOnBlock :");
+        // testBlockPileOnBlock(3,2);
+
     }
     
     //--------------------------------------------------------------------------------
     //TEST BLOCK ON PILE
-    private static void testBlockOnPile() {
+    private static void testBlockOnPile(int numBlocks, int numPiles) {
         // Paramètres pour le monde des blocs
-        int numBlocks = 2;
-        int numPiles = 2;
         // Création des actions pour le monde des blocs (BlockOnPile)
         BWActions2 blockWorldActions = new BWActions2(numBlocks, numPiles);
         Set<Action> allActions = blockWorldActions.getActions();
@@ -54,10 +59,10 @@ public class MainActions {
     private static Map<Variable, Object> createInitialStateBlockOnPile(int numBlocks, int numPiles) {
         Map<Variable, Object> initialState = new HashMap<>();
 
-        initialState.put(new Variable("On0", Set.of(-1)), -1);
-        initialState.put(new Variable("On1", Set.of(0)), 0);
-        initialState.put(new BooleanVariable("Fi0"), true);
-        initialState.put(new BooleanVariable("Fi1"), false);
+        initialState.put(new Variable("On0", Set.of(0, -1, 1)), 1);
+        initialState.put(new Variable("On1", Set.of(0, -1, 1)), -1);
+        initialState.put(new BooleanVariable("Fi0"), false);
+        initialState.put(new BooleanVariable("Fi1"), true);
         initialState.put(new BooleanVariable("Fr1"), false);
         initialState.put(new BooleanVariable("Fr2"), true);
 
@@ -96,18 +101,81 @@ public class MainActions {
         }
     }
 
-    // TEST BLOCK ON BLOCK
+    //--------------------------------------------------------------------------------
+    //TEST BLOCK ON BlOCK
+    private static void testBlockOnBlock(int numBlocks, int numPiles) {
+        // Paramètres pour le monde des blocs
+        // Création des actions pour le monde des blocs (BlockOnPile)
+        BWActions2 blockWorldActions = new BWActions2(numBlocks, numPiles);
+        Set<Action> allActions = blockWorldActions.getActions();
+        System.out.println(allActions.size());
+
+        // Création d'un état initial
+        Map<Variable, Object> initialState = createInitialStateBlockOnBlock(numBlocks, numPiles);
+
+        // Affichage de l'état initial
+        System.out.println("État initial pour BlockOnBlock :");
+        printState(initialState);
+        System.out.println();
+
+        // Test des actions avec l'état initial pour BlockOnPile
+        testActionsBlockOnBlock(initialState, allActions);
+
+        // Création d'un état final (but)
+        Map<Variable, Object> goalState = createGoalStateBlockOnBlock(numBlocks, numPiles);
+        System.out.println("État final :");
+        printState(goalState);
+        System.out.println();
+    }
+
+    private static Map<Variable, Object> createInitialStateBlockOnBlock(int numBlocks, int numPiles) {
+        Map<Variable, Object> initialState = new HashMap<>();
+
+        initialState.put(new Variable("On0", Set.of(0,-1,-2, 1,2)), -1);
+        initialState.put(new Variable("On1", Set.of(0,-1,-2, 1,2)), 0);
+        initialState.put(new Variable("On2", Set.of(0,-1,-2, 1,2)), -2);
+        initialState.put(new BooleanVariable("Fi0"), true);
+        initialState.put(new BooleanVariable("Fi1"), false);
+        initialState.put(new BooleanVariable("Fi2"), false);
+        initialState.put(new BooleanVariable("Fr1"), false);
+        initialState.put(new BooleanVariable("Fr2"), false);
+
+        return initialState;
+    }
+
     private static Map<Variable, Object> createGoalStateBlockOnBlock(int numBlocks, int numPiles) {
         Map<Variable, Object> goalState = new HashMap<>();
 
-        goalState.put(new Variable("On0", Set.of(-1)), -1);
-        goalState.put(new Variable("On1", Set.of(0)), 0);
+        goalState.put(new Variable("On0", Set.of(0,-1,-2, 1,2)), -1);
+        goalState.put(new Variable("On1", Set.of(0,-1,-2, 1,2)), 2);
+        goalState.put(new Variable("On2", Set.of(0,-1,-2, 1,2)), -2);
         goalState.put(new Variable("Fi0", Set.of(false)), false);
         goalState.put(new Variable("Fi1", Set.of(false)), false);
+        goalState.put(new Variable("Fi2", Set.of(false)), true);
         goalState.put(new Variable("Fr1", Set.of(false)), false);
         goalState.put(new Variable("Fr2", Set.of(false)), false);
 
         return goalState;
+    }
+
+    private static void testActionsBlockOnBlock(Map<Variable, Object> initialState, Set<Action> allActions) {
+        System.out.println("testActionsBlockOnBlock");
+        // Tester chaque action avec l'état initial
+        for (Action action : allActions) {
+            //System.out.println("Testing action: " + action);
+            
+            if (action.isApplicable(initialState)) {
+                System.out.println("Action is applicable.");
+                System.out.println(action.toString());
+                Map<Variable, Object> nextState = action.successor(initialState);
+                System.out.println("Resulting state:");
+                printState(nextState);
+                System.out.println();
+            } 
+            else {
+                // System.out.println("Action is not applicable.");
+            }
+        }
     }
 
     private static void printState(Map<Variable, Object> state) {
