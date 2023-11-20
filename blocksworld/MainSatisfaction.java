f4ee6f39e4d4d9310034499c911b9659d16ec3bb
erwann <erwann2002.ec@gmail.com>
2023-11-20 23:56:42 +0100

    main satisfaction avec croissant et regulier

--- blocksworld/MainSatisfaction.java
+++ blocksworld/MainSatisfaction.java
@@ -17,10 +17,10 @@ public class MainSatisfaction {
         System.out.println(" ################# TEST Contraintes Croissantes #################");
         TestSolverBlocksworld(bw2);
 
-        // BWConstraintesRegularyAndCroissant bw3 = new BWConstraintesRegularyAndCroissant(8, 3);
-        // bw3.allConstraints();
-        // System.out.println(" ################# TEST Contraintes Croissantes et Régulières #################");
-        // TestSolverBlocksworld(bw3);
+        BWRegularyConstraintes bw3 = new BWRegularyConstraintes(8, 3);
+        bw3.allConstraints();
+        System.out.println(" ################# TEST Contraintes Régulières #################");
+        TestSolverBlocksworld(bw3);
     }
 
         public static void TestSolverBlocksworld(BWConstraintes bw) {
@@ -77,30 +77,30 @@ public class MainSatisfaction {
         }
     }
 
-    //     public static void TestSolverBlocksworld(BWConstraintesRegularyAndCroissant bw) {
+        public static void TestSolverBlocksworld(BWRegularyConstraintes bw) {
 
-    //     Set<Variable> variables = new HashSet<>();
-    //     variables.addAll(bw.getBWVariable().getOnb());
-    //     variables.addAll(bw.getBWVariable().getFixedb());
-    //     variables.addAll(bw.getBWVariable().getFreep());
+        Set<Variable> variables = new HashSet<>();
+        variables.addAll(bw.getBWVariable().getOnb());
+        variables.addAll(bw.getBWVariable().getFixedb());
+        variables.addAll(bw.getBWVariable().getFreep());
 
-    //     Set<Solver> solvers = new HashSet<>();
-    //     Solver solver1 = new BacktrackSolver(variables, bw.getConstraints());
-    //     Solver solver2 = new MACSolver(variables, bw.getConstraints());
-    //     Solver solver3 = new HeuristicMACSolver(variables, bw.getConstraints(), new NbConstraintsVariableHeuristic(bw.getConstraints(), true), new RandomValueHeuristic(new Random()));
+        Set<Solver> solvers = new HashSet<>();
+        Solver solver1 = new BacktrackSolver(variables, bw.getConstraints());
+        Solver solver2 = new MACSolver(variables, bw.getConstraints());
+        Solver solver3 = new HeuristicMACSolver(variables, bw.getConstraints(), new NbConstraintsVariableHeuristic(bw.getConstraints(), true), new RandomValueHeuristic(new Random()));
 
-    //     solvers.add(solver1);
-    //     solvers.add(solver2);
-    //     solvers.add(solver3);
+        solvers.add(solver1);
+        solvers.add(solver2);
+        solvers.add(solver3);
 
-    //     for (Solver solver : solvers) {
-    //         System.out.println(" ################# TEST " + solver.getClass().getSimpleName() + " #################");
-    //         long startTime = System.nanoTime();
-    //         Map<Variable, Object> solution = solver.solve();
-    //         long endTime = System.nanoTime();
-    //         long duration = (endTime - startTime);
-    //         System.out.println("Temps de calcul : " + duration + " ns");
-    //         System.out.println("Solution : " + solution);
-    //     }
-    // }
+        for (Solver solver : solvers) {
+            System.out.println(" ################# TEST " + solver.getClass().getSimpleName() + " #################");
+            long startTime = System.nanoTime();
+            Map<Variable, Object> solution = solver.solve();
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);
+            System.out.println("Temps de calcul : " + duration + " ns");
+            System.out.println("Solution : " + solution);
+        }
+    }
 }
